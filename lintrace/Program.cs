b09d095657fd5aab1f87bdf00d8382c912ace3bd
Sergey Azarenko <azarenko.s@gmail.com>
2025-08-11 20:12:20 +0300

    no message

---
+++ lintrace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+
+namespace lintrace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baudRate = int.Parse(args[1]);
+
+            using (SerialPort port = new SerialPort(args[0], baudRate, Parity.None, 8, StopBits.One))
+            using (StreamWriter sw = new StreamWriter("trace.log"))
+            {
+
+            }
+        }
+    }
+}
